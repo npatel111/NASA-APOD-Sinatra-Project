661c23f2b1768a8a42546e978669cb7d6945cc75
Niti Nrw <niti.patel@flatironschool.com>
2016-12-29 19:30:15 -0600

    All three controllers have full CRUD actions and communication with each other

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,38 @@
 class MessagesController < ApplicationController
+  get '/messages' do
+    @messages = Message.all
+    erb :"/messages/index"
+  end
 
-end  
+  get '/messages/new' do
+    erb :'messages/new'
+  end
+
+  post '/messages' do
+    @message = Message.create(params)
+    redirect to "/messages/#{@message.id}"
+  end
+
+  get '/messages/:id' do
+    # byebug
+    @message = Message.find(params[:id])
+    erb :'/messages/show'
+  end
+
+  get '/messages/:id/edit' do
+    @message = Message.find(params[:id])
+    erb :'/messages/edit'
+  end
+
+  post '/messages/:id' do
+    @message = Message.find(params[:id])
+    @message.update(params["message"])
+    redirect to "/messages/#{@message.id}"
+  end
+
+  post '/messages/:id/delete' do
+    @message = Message.find(params[:id])
+    @message.delete
+    redirect to '/messages'
+  end
+end
